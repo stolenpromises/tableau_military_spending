18e74691fd89a0a486ea01e94ff017019542950c
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-12-13 14:55:46 -0600

    Initial commit

---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 12:35:11 2021
+
+@author: nathan.m
+"""
+
+import pandas as pd
+countries = pd.read_csv (r'O:/OneDrive/education/tableau/military_spending/countries.csv')
