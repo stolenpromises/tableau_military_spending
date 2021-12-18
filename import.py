cccbcfc3134865407e0b605a2604cc92bbcde5b5
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-12-18 15:10:51 -0600

    Beginning draft of import docstrings

---
+++ import.py
@@ -0,0 +1,38 @@
+"""Code written while learning beginner level pandas.
+
+In completing data_cleanup.py, I'm realizing I will probably need further
+abstraction.
+
+Created on Sat Dec 18 13:14:19 2021
+
+@author: nathan.m
+"""
+import pandas as pd
+import numpy as np
+import os
+class AliasLookup(object):
+    """Collection of country names along with known aliases."""
+    def __init__(self, filename):
+        """Instatiates an ALiasLoopup object instance.
+
+        Stores a pandas DataFrame of countries as imported from a csv file.
+
+        Parameters
+        ----------
+        filename : STR
+            Name of the csv file.
+        Returns
+        -------
+        None.
+
+        """
+
+        def add_country(self, country, aliases:
+            """ Append a country to AliasLookup.
+            
+        def clr_country:
+        def get_alias: 
+        def set_alias:
+        
+    
+"""
